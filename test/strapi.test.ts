e756953a009242ebc01d859a7054361a167aff60
henrik-clabs <henrik.sorensen@cinfinitysolutions.com>
2024-11-12 07:01:20 +0000

    test strapi: always return status never throw

--- test/strapi.test.ts
+++ test/strapi.test.ts
@@ -7,6 +7,12 @@ var properties = propertiesReader(".env.local")
 const options: StrapiClientArgs = {
   baseUrl: properties.get("STRAPI_URL"),
   apiKey: properties.get("STRAPI_API_KEY"),
+  axiosOptions:{ // https://axios-http.com/docs/req_config
+  validateStatus: function (status: number) {
+    //console.log("validateStatus ",status)
+    return true // always return a status. Never throw errors.
+    //return status >= 200 && status < 999; // always return a status. Never throw errors.
+  }},
 }
 
 var client:Strapi
@@ -82,32 +88,43 @@ describe('Strapi CRUD', () => {
 
 
 test('basic', async () => {
-    try{
-        const result = await client.findOne("test",0)
-    } catch (error: any) {
-        // if (error && typeof(error)=='object') {
-        // console.log("Error", Object.keys(error),error['name'],error['code'],error['address'])
-        // if (error['name']=="AggregateError") {
-        //     console.log("AggregateError[0]",Object.keys(error.errors[0]),error.errors[0].errno,error.errors[0].code,error.errors[0].address,error.errors[0].port)
-        //     console.log("AggregateError[1]",Object.keys(error.errors[1]),error.errors[1].errno,error.errors[1].code,error.errors[1].address,error.errors[1].port)
-        // }
-        // }
-        const b=(error['code'] == "ECONNREFUSED" || error['code'] == "ERR_BAD_REQUEST")
-        expect(b).toBeTruthy()
-    }
-  });
+      const result = await client.findOne("test",0)
+      expect(result.status).toBe(404)  // test api: 404 Not found 
+
+      // Check testuser was created
+      const result1 = await client.findAll("authors",as_filter(eq("nameX","Sdk Testuser")))
+      console.log("result1", Object.keys(result1), result1.data)
+      expect(result1.status).toBe(400) // 400 validation error. Invalid key nameX
+
+      // result1 [ 'data', 'status' ] 
+      // result1.data {
+      //   data: null,
+      //   error: {
+      //     status: 400,
+      //     name: 'ValidationError',
+      //     message: 'Invalid key nameX',
+      //     details: { key: 'nameX', path: null, source: 'query', param: 'filters' }
+      //   }
+      // }
+
+    });
 
 
   test('basic-author', async () => {
         const result0 = await client.findAll("authors",as_filter(eq("name","David Doe")))
+        expect(result0.status).toBe(200)
         expect(result0.data.data.length).toBe(1)
         const result1 = await client.findAll("authors",as_filter(eq("name","David DoeXX")))
+        expect(result1.status).toBe(200)
         expect(result1.data.data.length).toBe(0)
         const result2 = await client.findAll("authors",as_filter(eq("name","Sarah Baker")))
+        expect(result2.status).toBe(200)
         expect(result2.data.data.length).toBe(1)
         const result3 = await client.findAll("authors",as_filter(and(eq("name","David Doe"),eq("name","Sarah Baker"))))
+        expect(result3.status).toBe(200)
         expect(result3.data.data.length).toBe(0)
         const result4 = await client.findAll("authors",as_filter(or(eq("name","David Doe"),eq("name","Sarah Baker"))))
+        expect(result4.status).toBe(200)
         expect(result4.data.data.length).toBe(2)
 });
 
@@ -119,6 +136,7 @@ test('basic-author-crud', async () => {
 
     // Check test user does not exist
     const result0 = await client.findAll("authors",as_filter(eq("name",data.name)))
+    expect(result0.status).toBe(200)
     expect(result0.data.data.length).toBe(0)
 
     // Create testuser
@@ -129,6 +147,7 @@ test('basic-author-crud', async () => {
 
     // Check testuser was created
     const result2 = await client.findAll("authors",as_filter(eq("name","Sdk Testuser")))
+    expect(result2.status).toBe(200)
     expect(result2.data.data.length).toBe(1)
 
     const data1 = {
@@ -136,28 +155,35 @@ test('basic-author-crud', async () => {
         "email": "sdktestuser@noemail.com", // change email only
     }
     const result3 = await client.update("authors",result1.data.data.documentId, {data:data1} )
-    //console.log(result3)
     expect(result3.status).toBe(200)
 
     // Check update happened
     const result4 = await client.findAll("authors",as_filter(eq("name","Sdk Testuser")))
+    expect(result4.status).toBe(200)
     expect(result4.data.data.length).toBe(1)
     expect(result4.data.data[0].email).toBe(data1.email)
     expect(result4.data.data[0].documentId).toBe(result1.data.data.documentId)
 
     // Check update happened
     const result5 = await client.findOne("authors",result1.data.data.documentId)
-    //console.log(result5)
     expect(result5.status).toBe(200)
     expect(result5.data.data.name).toBe(data.name)
     expect(result5.data.data.name).toBe(data1.name)
     expect(result5.data.data.email).toBe(data1.email)
+    expect(result5.data.data.email).not.toBe(data.email)
 
 
     // Delete testuser
     const result6 = await client.delete("authors",result1.data.data.documentId)
+    //console.log(result6)
     expect(result6.status).toBe(204)
     expect(result6.deleted).toBeTruthy()
+
+    // // elete testuser again should fail
+    // const result6a = await client.delete("authors",result1.data.data.documentId)
+    // // console.log(result6a)
+    // expect(result6a.status).not.toBe(204)
+    // expect(result6a.deleted).not.toBeTruthy()
     
     // Check Delete happened
     const result7 = await client.findAll("authors",as_filter(eq("name",data.name)))
